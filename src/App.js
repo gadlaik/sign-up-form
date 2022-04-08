598f82e933620f0b580fb36233777136f681557a
gadlaik <sabatonz@hotmail.com>
2022-04-08 23:15:23 +0200

    few form functionalities added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import "./App.css";
 import logo from "./assets/campfire-logo-gng.png";
 
 function App() {
+  const form = document.querySelector(".sign-up-form");
+  const passWarning = document.querySelector(".pass-warning");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    form.password.value === form.confirmPass.value
+      ? passWarning.classList.remove("show")
+      : passWarning.classList.add("show");
+    !passWarning.classList.contains("show") && form.reset();
+  };
+
   return (
     <div className="App">
       <header>
@@ -9,20 +21,26 @@ function App() {
         <h2>Primitive Survival School</h2>
       </header>
 
-      <form className="sign-up-form" autoComplete="off">
+      <form className="sign-up-form" autoComplete="off" onSubmit={handleSubmit}>
         <div className="input-field">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" name="firstName" id="first-name" />
+          <input
+            type="text"
+            name="firstName"
+            id="first-name"
+            minLength={3}
+            required
+          />
         </div>
 
         <div className="input-field">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" name="lastName" id="last-name" />
+          <input type="text" name="lastName" id="last-name" required />
         </div>
 
         <div className="input-field">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <input type="email" name="email" id="email" required />
         </div>
 
         <div className="input-field">
@@ -45,20 +63,21 @@ function App() {
 
         <div className="input-field">
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
+          <input type="password" name="password" id="password" required />
         </div>
 
         <div className="input-field">
           <label htmlFor="confirm-pass">Confirm Password</label>
-          <input type="password" name="confirmPass" id="confirm-pass" />
+          <input
+            type="password"
+            name="confirmPass"
+            id="confirm-pass"
+            required
+          />
         </div>
+        <p className="pass-warning">Passwords don't match</p>
 
-        <input
-          type="submit"
-          value="Create Account"
-          className="submit-btn"
-          disabled
-        />
+        <input type="submit" value="Create Account" className="submit-btn" />
 
         <p>
           Already have an account? <a href="/">Log in</a>
